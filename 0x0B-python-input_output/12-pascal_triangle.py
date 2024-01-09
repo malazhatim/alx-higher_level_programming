54ca9989f3a17248588d2e761c079c2974813709
malazhatim <malaz.hatim@gmail.com>
2024-01-09 15:07:19 -0800

    12

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -13,14 +13,14 @@ def pascal_triangle(n):
     if n <= 0:
         return []
     res = []
-    l = []
+    a = []
     for x in range(n):
         row = []
         for y in range(x + 1):
             if x == 0 or y == 0 or x == y:
                 row.append(1)
             else:
-                row.append(l[y] + l[y - 1])
-        l = row
+                row.append(a[y] + a[y - 1])
+        a = row
         res.append(row)
     return res
